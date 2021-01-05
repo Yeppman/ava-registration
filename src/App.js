e61d5dc910dc98d0e137d520525d73881fabd5f0
taiwoadebayo4040@gmail.com <taiwoadebayo4040@gmail.com>
2021-01-05 13:11:17 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -1,67 +1,263 @@
-import React, { useState } from 'react';
-const api = {
-  key: "429736441cf3572838aa10530929f7cd",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+import React, { useState, useEffect } from "react";
+import { GiSoccerBall } from "react-icons/gi";
+import axios from "axios";
+// import NavbarTwo from "../components/navbar-two";
+import { Input, Form, Select, Upload, Tabs, message } from "antd";
+const { TabPane } = Tabs;
 
-function App() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
+import { InboxOutlined } from "@ant-design/icons";
 
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
+const { Dragger } = Upload;
+const { Option } = Select;
+
+const SectionBoxes = () => {
+  const [isPlayer, setisPlayer] = useState(false);
+  const [registeringCategory, setRegisteringCategory] = useState("Player");
+  const [videoFile ,setVideo] = useState([])
+  const Coach = "Coach";
+  const Scout = "Scout";
+
+  function userIsPlayer() {
+    setisPlayer(true);
+    setRegisteringCategory("Player");
+    console.log(isPlayer)
+  }
+
+  function dontUserPlayer(Val) {
+    setisPlayer(false);
+    setRegisteringCategory(Val);
+    console.log(Val);
+  }
+
+  const sendData = async (values) => {
+   
+    // const host = "https://avalanche-backend.herokuapp.com";
+    const host = 'http://127.0.0.1:8000'
+    const endpoint = host + `/core/reg-upload/`;
+
+    const fName =  values['fName']
+    const lName =  values['lName']
+    const email =  values['Email']
+    const Position = values['Position']
+    const Phone = values['Phone']
+
+    let videoUpload = videoFile
+    let Age = 0;
+
+    if (registeringCategory == Coach || registeringCategory == Scout) {
+    } else {
+      // videoUpload = e.target.elements.playerVideo.files;
+      // Age = e.target.elements.age.value;
     }
-  }
+    const Category = registeringCategory;
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    console.log(fName, lName, email, Phone, videoUpload, Category);
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    let fd = new FormData();
+    fd.append("fName", fName);
+    fd.append("lName", lName);
+    fd.append("email", email); 
+    fd.append("phone", Phone);
+    fd.append('Position',Position)
+    fd.append("videoUpload", videoUpload);
+    fd.append("category", Category);
+    fd.append("age", Age);
 
-    return `${day} ${date} ${month} ${year}`
-  }
+    axios.post(endpoint, fd).then((res) => {
+      if (res.status == 200) {
+        message.success("Registration Successfully");
+      } else {
+        message.error("Registration Failed ");
+      }
+    });
+  };
+
+  // Video
+  
+const props = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    console.log(info.file)
+    if (status !== 'uploading') {
+      console.log('the file',info.file);
+      setVideo(info.fileList[0].originFileObj)
+      console.log('file list' ,info.fileList[0].originFileObj )
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      // message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+
 
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-      <main>
-        <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
+    <>
+      <div className="container">
+      <div className="">
+                <div className="">
+                    <p className="generalHeading">
+                      Sign Up Today 
+                      </p>
+                </div>
+              <Form
+                className=""
+                onFinish={sendData}
+              >
+                <Form.Item rules={[{ required: true }]} name="fName">
+                  <Input
+                    size="large"
+                    placeholder="Your First Name?"
+                    enterButton
+                  />
+                </Form.Item>
+                <Form.Item rules={[{ required: true }]} name="lName">
+                  <Input
+                    size="large"
+                    placeholder="Your Last Name?"
+                    enterButton
+                  />
+                </Form.Item>
+
+                <Form.Item rules={[{ required: true }]} name="Email">
+                  <Input size="large" placeholder="Your Email?" enterButton />
+                </Form.Item>
+                
+                <Form.Item rules={[{ required: true }]} name="Phone">
+                  <Input size="large" placeholder="Your Email?" enterButton />
+                </Form.Item>
+
+                <Form.Item rules={[{ required: true }]} name="Nationality">
+                  <Input size="large" placeholder="Nationality?" enterButton />
+                </Form.Item>
+
+                <Form.Item rules={[{ required: true }]} name="Position">
+                  <Input
+                    size="large"
+                    placeholder="Playing Position"
+                    enterButton
+                  />
+                </Form.Item>
+                
+                {/* <Form.Item
+                    rules={[{ required: true }]}
+                    name="State"
+                    hasFeedback
+                  >
+                    <Select size="large" placeholder="Select Your Role">
+                    <Option
+                    onSelect={()=>{userIsPlayer()}}
+                    value='Player'>
+                      Player
+                    </Option>
+                    <Option
+                      onSelect={() => {
+                        dontUserPlayer(Coach);
+                      }}
+                    value='Coach'>
+                      Coach
+                    </Option>
+                    <Option
+                    onSelect={() => {
+                      dontUserPlayer(Scout);
+                    }}
+                    value='Scout'>
+                      Scout
+                    </Option>
+                      
+                    </Select>
+                  </Form.Item> */}
+
+                <Form.Item>
+                  <div className="reg-category-container">
+                    <h3 className="reg-category-heading">
+                      
+                      </h3>
+                    <ul className="reg-category-list">
+                      <li onClick={userIsPlayer} className="reg-category-item">
+                        <div className="reg-category-icon">
+                          <GiSoccerBall />
+                        </div>
+                        <p className="reg-category-text">Player</p>
+                      </li>
+
+                      <li
+                        onClick={() => {
+                          dontUserPlayer(Coach);
+                        }}
+                        className="reg-category-item"
+                      >
+                        <div className="reg-category-icon">
+                          <GiSoccerBall />
+                        </div>
+                        <p className="reg-category-text">Scout</p>
+                      </li>
+
+                      <li
+                        onClick={() => {
+                          dontUserPlayer(Scout);
+                        }}
+                        className="reg-category-item"
+                      >
+                        <div className="reg-category-icon">
+                          <GiSoccerBall />
+                        </div>
+                        <p className="reg-category-text">Coach</p>
+                        
+                      </li>
+                    </ul>
+                  </div>
+
+                  {isPlayer ? (
+                    <>
+                      <Dragger {...props}>
+                        <p className="ant-upload-drag-icon">
+                          <InboxOutlined />
+                        </p>
+                        <p className="ant-upload-text">
+                          Click or drag file to this area to upload
+                        </p>
+                        <p className="ant-upload-hint">
+                          Support for a single or bulk upload. Strictly prohibit
+                          from uploading company data or other band files
+                        </p>
+                      </Dragger>
+                    ]
+                 
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                  <button
+                    type="submit"
+                    htmltype="submit"
+                    className="registerbtn"
+                  >
+                    Sign Up
+                  </button>
+                </Form.Item>
+              </Form>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
-          </div>
+      </div>
+     
+    </>
+  );
+};
+
+export default function signUpForm() {
+  return (
+    <>
+      {/* <NavbarTwo /> */}
+      <div className="playSignUp-container">
+        <div className="playSignUp-form-fields">
+          <SectionBoxes />
         </div>
-        ) : ('')}
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
-
-export default App;
