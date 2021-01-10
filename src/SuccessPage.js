86f4b8aa803e93872f4ceb3b56a9f4890dae8d72
Yeppman <yeppman13@gmail.com>
2021-01-10 15:57:15 +0000

    AA

--- src/SuccessPage.js
+++ src/SuccessPage.js
@@ -11,15 +11,19 @@ export default function SuccessPage() {
                     <p className="successHeading">
                           Welcome to BallHive!
                            <br/>
+                          <p
+                          style={{fontSize:20}}
+                          className="successHeading">
+                            Your registration was successful
+                        </p>
+                       
                       
                     </p>
                     <p className="successText">
                         Thank you for registering with us. We are currently building our mobile app with all the features to help players
                         showcase themselves and help scouts discover talents.
                        <br />
-
-                    <b>NOTE :</b>This registration is to keep you updated about our progress, give you an early
-                    advantage upon our product launch, and also bring special opportunities to you.
+                        <b>NOTE:</b> This is a pre-product sign up to keep you updated about our product launch.
                     </p>
                     <a href="https://sportsavalanche.com/">
                         <button
